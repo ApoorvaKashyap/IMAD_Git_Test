1a9ed532121016d8bdd500869e50ad069631112c
ApoorvaKashyap <ApoorvaKashyap@users.noreply.github.com>
2017-09-21 20:36:48 +0530

    New Feature Function:Halfway through the code

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+function  newFeature()
+{
+	console.log('Adding new feature');
+	console.log('Halfway Done');
+}
 function smthng()
 {
 	console.log('New Func');
 }
+
 console.log('hi');
 smthng();
+newFeature();
